0734b223ba7fed36bf94dbfc43da382b6cfdc5bc
Lucía Flores López <lflores@ifae.es>
2026-05-08 17:26:59 +0200

    Updated w1_w2.py and PowerSpectrum

--- MW11model/w1_w2.py
+++ MW11model/w1_w2.py
@@ -2,6 +2,8 @@ import scipy.constants as scipy_constants
 import scipy.special
 from picca import constants
 import numpy as np
+import argparse
+import h5py
 
 def voigt(x, sigma=1, gamma=1):
     return np.real(scipy.special.wofz((x + 1j*gamma)/(sigma*np.sqrt(2))))
@@ -22,7 +24,6 @@ def get_voigt_profile_wave(wave, z, logNHi):
         array
             Flux corresponding to a voigt profile
     """
-
 
     e = 1.6021e-19  # C
     epsilon0 = 8.8541e-12  # C^2.s^2.kg^-1.m^-3
@@ -66,6 +67,7 @@ def profile_wave_to_comov_dist(wave, profile_wave, omegam):
 
     return r_linspace, profile_lin
 
+
 def fft_profile(profile, dx):
     """Compute Fourier transform of a voigt profile 
 
@@ -87,6 +89,7 @@ def fft_profile(profile, dx):
     k = np.fft.rfftfreq(size, dx) * (2 * np.pi) # 2pi factor is to obtain k in h/Mpc^-1 instead of in frecuency values
  
     return k, np.abs(ft_profile)
+
   
 def wave_to_fft_profile(wave, z, logNHi, omegam):
     """Computes Fourier transform of a given wave (observed wavelenght) at input redshift and logNHi.
@@ -115,7 +118,6 @@ def wave_to_fft_profile(wave, z, logNHi, omegam):
         (wavenumber grid, voigt profile in Fourier space, dx)"""
 
 
-    
     profile_wavelength = get_voigt_profile_wave(wave, z, logNHi)
     profile_wavelength /= np.mean(profile_wavelength) 
     lin_spaced_cmv, profile_cmv = profile_wave_to_comov_dist(wave, profile_wavelength, omegam)
@@ -153,4 +155,124 @@ def resample_to_logk(k, w, k_min=None, k_max=None):
     return k_log, w_log
 
 
+
+def fNHiX_to_fn(fNHiX, NHirange):
+    """
+    Function to change from f(NHi, X) -column density distribution function per absorption length- to f(n) in Tan25 (normalized)
+     
+    Parameters
+    -----------
+    fNHiX : array
+        f(NHi, X) function in terms of NHi range [cm^2]
+    NHirange : array
+        log10 (column density values [cm^-2]) 
     
+    Returns
+    -----------
+    (array)
+        f(n) function in terms of NHirange"""
+    
+    fn = []
+    for i in np.arange(len(NHirange)):
+        fn.append(fNHiX[i] * 10**(NHirange[i]))
+    fn = np.array(fn) * np.log(10)
+    fn /=  np.trapezoid(fn, NHirange)
+
+    return fn
+
+
+
+def fn_to_fNHi(fn, NHirange, dX, norm=False, norm_factor=None):
+    """
+    Function to change from f(n) in Tan25 to f(NHi, X) -column density distribution function per absorption length
+    
+    Parameters
+    -------------
+    fn : array
+        f(n) function from Tan25 [unitless]
+    NHirange : array
+        log10 (column density values [cm^-2]) the function f(n) is evaluated at
+    dX : array
+        Total absorption length
+    norm : bool
+        If True, f(n) input has been normalized to the area.
+        If False, f(n) has not been noralized (counts/bin_width)
+    norm_factor : float, optional
+        Required if norm=True
+        Value used to normalized f(n) (integrated area of unnormalized f(n))
+
+    Returns
+    ----------
+    array
+        f(Nhi, X) function
+    """
+
+    if norm and norm_factor is None:
+        raise ValueError('norm_factor must be provided when norm=True')
+    
+    if norm:
+        fn *= norm_factor
+    
+    fnX = fn/dX
+    fNHi = fnX/(np.log(10)*(10**NHirange))
+
+    return fNHi
+
+
+
+def compute_w1_w2(wave, NHirange, fNHiX, z, hubble, omegam, resample=True):
+    """ Function to compute w1 and w2 following MW11 paper
+    
+    Parameters
+    ------------
+    wave : array
+        Wavelenght values [A] to compute the Voigt profile
+    NHirange : array
+        log10(Column density [cm^-2]) array
+    fNHIX : array
+        f(NHi, X) -column density distribution function per absorption lenght- evaluated at NHirange
+    z : float
+        Redshift
+    hubble : float
+        Little hubble constant h
+    omegam : float
+        omega matter density
+    resample : bool
+        Wether the user wants the outputs resampled into logarithmic spaced k
+    
+    Returns
+    ---------
+    (array, array, array)
+        (k_los [Mpc^-1], w1 [Mpc], w2 [Unitless])
+    
+    """
+ 
+    # Conversion factor dXdComov:
+    c_kms = scipy_constants.speed_of_light*1e-3  # km/s
+    conversion_factor = 100*hubble*((1+z)**2)/c_kms
+    cddf = conversion_factor*fNHiX
+
+    # Computation of w1 and w2
+    w1_int, w2_int = [], []
+    for i, nhi_value in enumerate(NHirange):
+        k_w1w2, d, Deltax = wave_to_fft_profile(wave, z, nhi_value, omegam=omegam)  # k is in h/Mpc and d in Mpc/h
+        k_w1w2 /= hubble  # [Mpc^-1]
+        d *= hubble  # [Mpc]
+        w2_int.append(cddf[i]*d*np.log(10)*(10**nhi_value))
+        w1_int.append(cddf[i]*(d**2)*np.log(10)*(10**nhi_value))
+
+    w1_int, w2_int = np.array(w1_int), np.array(w2_int)
+    w1 = np.trapezoid(w1_int, NHirange, axis=0)
+    w2 = np.trapezoid(w2_int, NHirange, axis=0)
+
+    if resample:
+        k_los, w1 = resample_to_logk(k_w1w2, w1, k_max=np.pi/(Deltax*2))
+        k_los, w2 = resample_to_logk(k_w1w2, w2, k_max=np.pi/(Deltax*2))
+
+        return k_los, w1, w2
+
+        
+    else:
+        return k_w1w2, w1, w2
+    
+
