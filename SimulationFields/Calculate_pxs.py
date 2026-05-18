d5417afc8c76c208ea8184d8449bc02fc45efcca
lfloreslop <lflores@ifae.es>
2026-05-18 01:26:26 -0700

    Fixed bugs

--- SimulationFields/Calculate_pxs.py
+++ SimulationFields/Calculate_pxs.py
@@ -3,9 +3,6 @@ import matplotlib.pyplot as plt
 import h5py
 import glob
 import argparse
-<<<<<<< HEAD
-from Calculate_deltas import mask_skewers
-=======
 
 def px_calc(r_bins, row_in, col_in, Nsk, colden_grid, fft_A, fft_B, prt=True):
     if prt:
@@ -37,30 +34,20 @@ def px_calc(r_bins, row_in, col_in, Nsk, colden_grid, fft_A, fft_B, prt=True):
         px_tot.append(np.mean(px, axis=0))
         
     return np.array(px_tot)  
->>>>>>> wip_px
 
 def main(args):
     Nmbox = len(glob.glob(args.folder_ffts + '/*.hdf5'))
     print(Nmbox, 'Miniboxes found')
 
-<<<<<<< HEAD
     filename = glob.glob(args.folder_ffts + '/*.hdf5')[0]
     with h5py.File(filename, 'r') as f:
         k_los = f['k_los'][:]
-        
 
-    px_tot = np.zeros(shape=(Nmbox, args.num_rbins+1, len(k_los)))
-    px_lya, px_hcd, px_lyahcd = np.zeros_like(px_tot), np.zeros_like(px_tot), np.zeros_like(px_tot)
-    px_3lya, px_3hcd, px_4 = np.zeros_like(px_tot), np.zeros_like(px_tot), np.zeros_like(px_tot)
-    C = []
-    
-=======
     px_tot = []
     px_lya, px_hcd, px_lyahcd = [], [], []
     px_3lya, px_3hcd, px_4 = [], [], []
     C = []
 
->>>>>>> wip_px
     mb_index = 0
     for filename in sorted(glob.glob(args.folder_ffts + '/*.hdf5')):
         print('Minibox', mb_index, '...')
@@ -80,13 +67,6 @@ def main(args):
             Ssk = f.attrs['skewer_separation_Mpch']
 
             # Data
-<<<<<<< HEAD
-            fft_tot = f['fft_tot'][:]
-            fft_lya = f['fft_lya'][:]
-            fft_hcd = f['fft_hcd'][:]
-            fft_lyahcd = f['fft_lyahcd'][:]
-            k_los = f['k_los'][:]
-=======
             fft_tot = f['fft_tot'][:, :args.index_max]
             fft_lya = f['fft_lya'][:, :args.index_max]
             fft_hcd = f['fft_hcd'][:, :args.index_max]
@@ -94,94 +74,10 @@ def main(args):
             k_los = f['k_los'][:args.index_max]
             if mb_index == 0:
                 print('klos from', k_los[0], 'to', k_los[-1], 'Mpc^-1')
->>>>>>> wip_px
             C_mb = f['C'][()]
             C.append(C_mb)
             colden = f['colden'][:]
 
-<<<<<<< HEAD
-        # Computing r distances
-        ix, iy = np.divmod(np.arange(Nsk), Lmbox)  # This gives me the coordinates of each skewer within the minibox
-        dx = ix[:, None] - ix[None, :]
-        dy = iy[:, None] - iy[None, :]
-        dr = np.sqrt(dx**2 + dy**2)*Ssk  # This matrix contains the radial distance (Mpc/h) between all skewers within the minibox
-
-        # Setting r bins (the same for all miniboxes)
-        if mb_index == 0:
-            r_bins = np.linspace(dr.min(), dr.max(), args.num_rbins+1)  # radial bins Mpc/h
-            print(len(r_bins), 'radial edges (Mpc/h) defined to have', len(r_bins)-1, 'radial bins:', r_bins, '[Mpc/h]')
-
-        # Masking
-        _, _, (fft_tot_mask, fft_lya_mask, fft_hcd_mask, fft_lyahcd_mask, dr_flatten_mask) = mask_skewers(colden, 
-                                                                                                          args.logNHi_min, args.logNHi_max, Nsk, 
-                                                                                                          fft_tot, fft_lya, fft_hcd, fft_lyahcd, dr.flatten())
-
-        # Computing Px contributions and mean values
-        # Total
-        for i, r_value in enumerate(r_bins[:-1]):
-            r_mask = (dr_flatten_mask > r_value) & (dr_flatten_mask < r_bins[i+1])
-            fft_tot_2mask = fft_tot_mask[r_mask]
-    
-            pxs_tot = fft_tot_2mask*fft_tot_2mask.conjugate()*Lbox/(Np**2)
-            px_tot[mb_index, i, :] = np.mean(pxs_tot, axis=0)
-
-        # Lya
-        for i, r_value in enumerate(r_bins[:-1]):
-            r_mask = (dr_flatten_mask > r_value) & (dr_flatten_mask < r_bins[i+1])
-            fft_lya_2mask = fft_lya_mask[r_mask]
-    
-            pxs_lya = fft_lya_2mask*fft_lya_2mask.conjugate()*Lbox/(Np**2)
-            px_lya[mb_index, i, :] = np.mean(pxs_lya, axis=0)
-
-        # HCDs
-        for i, r_value in enumerate(r_bins[:-1]):
-            r_mask = (dr_flatten_mask > r_value) & (dr_flatten_mask < r_bins[i+1])
-            fft_hcd_2mask = fft_hcd_mask[r_mask]
-    
-            pxs_hcd = fft_hcd_2mask*fft_hcd_2mask.conjugate()*Lbox/(Np**2)
-            px_hcd[mb_index, i, :] = np.mean(pxs_hcd, axis=0)
-
-        # Lya x HCDs
-        for i, r_value in enumerate(r_bins[:-1]):
-            r_mask = (dr_flatten_mask > r_value) & (dr_flatten_mask < r_bins[i+1])
-            fft_lya_2mask = fft_lya_mask[r_mask]
-            fft_hcd_2mask = fft_hcd_mask[r_mask]
-    
-            pxs_lyahcd = fft_lya_2mask*fft_hcd_2mask.conjugate()*Lbox/(Np**2)
-            px_lyahcd[mb_index, i, :] = np.mean(pxs_lyahcd, axis=0)
-
-        # 3Lya
-        for i, r_value in enumerate(r_bins[:-1]):
-            r_mask = (dr_flatten_mask > r_value) & (dr_flatten_mask < r_bins[i+1])
-            fft_lya_2mask = fft_lya_mask[r_mask]
-            fft_lyahcd_2mask = fft_lyahcd_mask[r_mask]
-    
-            pxs_3lya = fft_lyahcd_2mask*fft_lya_2mask.conjugate()*Lbox/(Np**2)
-            px_3lya[mb_index, i, :] = np.mean(pxs_3lya, axis=0)
-
-        # 3HCD
-        for i, r_value in enumerate(r_bins[:-1]):
-            r_mask = (dr_flatten_mask > r_value) & (dr_flatten_mask < r_bins[i+1])
-            fft_hcd_2mask = fft_hcd_mask[r_mask]
-            fft_lyahcd_2mask = fft_lyahcd_mask[r_mask]
-    
-            pxs_3hcd = fft_lyahcd_2mask*fft_hcd_2mask.conjugate()*Lbox/(Np**2)
-            px_3hcd[mb_index, i, :] = np.mean(pxs_3hcd, axis=0)
-
-        # 4
-        for i, r_value in enumerate(r_bins[:-1]):
-            r_mask = (dr_flatten_mask > r_value) & (dr_flatten_mask < r_bins[i+1])
-            fft_lyahcd_2mask = fft_lyahcd_mask[r_mask]
-    
-            pxs_4 = fft_lyahcd_2mask*fft_lyahcd_2mask.conjugate()*Lbox/(Np**2)
-            px_4[mb_index, i, :] = np.mean(pxs_4, axis=0)
-        
-        mb_index += 1
-        
-    C = np.array(C)
-
-    # Writing the files out
-=======
         # Reshaping fft arrays:
         fft_tot_grid = fft_tot.reshape(Nsk, Nsk, len(k_los))
         fft_lya_grid = fft_lya.reshape(Nsk, Nsk, len(k_los))
@@ -257,7 +153,6 @@ def main(args):
     C = np.array(C)
     
    # Writing the files out
->>>>>>> wip_px
     with h5py.File(f'{args.output_file}.hdf5', 'w') as f:
     
         f.attrs['logNHI_min'] = logNHi_min
@@ -286,11 +181,6 @@ def main(args):
         f.create_dataset('px_4', data=px_4)
 
     print(f'Results saved in {args.output_file}.hdf5')
-
-<<<<<<< HEAD
-=======
-
->>>>>>> wip_px
     
     return
 
@@ -301,15 +191,9 @@ if __name__ == "__main__":
 
     parser.add_argument("--folder_ffts", type=str, required=True,
                         help="Folder witht the HDF5 files with ffts of the different fields for each minibox")
-<<<<<<< HEAD
-    parser.add_argument("--num_rbins", type=int, required=True,
-                        help="Number of bins the user wants to divide r into")
-    parser.add_argument("--logNHi_min", type=float, default=-10,
-=======
     parser.add_argument("--index_max", type=int, required=True,
                         help="Up to what index the user needs to read k_los")
     parser.add_argument("--logNHi_min", type=float, default=0,
->>>>>>> wip_px
                         help="Minimum log10 column density")
     parser.add_argument("--logNHi_max", type=float, required=True,
                         help="Maximum log10 column density")
